a9eb660ca3a554c08d0510cdbe06671f84bee077
dkazanc <dkazanc@hotmail.com>
2025-06-04 14:36:58 +0100

    removes library scripts for remove_paganin_savu

--- httomo_backends/methods_database/packages/backends/httomolibgpu/supporting_funcs/prep/phase.py
+++ httomo_backends/methods_database/packages/backends/httomolibgpu/supporting_funcs/prep/phase.py
@@ -27,69 +27,8 @@ import numpy as np
 from httomo_backends.cufft import CufftType, cufft_estimate_2d
 
 __all__ = [
-    "_calc_memory_bytes_paganin_filter_savu",
     "_calc_memory_bytes_paganin_filter_tomopy",
 ]
-
-
-def _calc_memory_bytes_paganin_filter_savu(
-    non_slice_dims_shape: Tuple[int, int],
-    dtype: np.dtype,
-    **kwargs,
-) -> Tuple[int, int]:
-    pad_x = kwargs["pad_x"]
-    pad_y = kwargs["pad_y"]
-
-    # Input (unpadded)
-    unpadded_in_slice_size = np.prod(non_slice_dims_shape) * dtype.itemsize
-
-    # Padded input
-    padded_non_slice_dims_shape = (
-        non_slice_dims_shape[0] + 2 * pad_y,
-        non_slice_dims_shape[1] + 2 * pad_x,
-    )
-    padded_in_slice_size = (
-        padded_non_slice_dims_shape[0] * padded_non_slice_dims_shape[1] * dtype.itemsize
-    )
-
-    # Padded input cast to `complex64`
-    complex_slice = padded_in_slice_size / dtype.itemsize * np.complex64().nbytes
-
-    # Plan size for 2D FFT
-    fftplan_slice_size = cufft_estimate_2d(
-        nx=padded_non_slice_dims_shape[1],
-        ny=padded_non_slice_dims_shape[0],
-        fft_type=CufftType.CUFFT_C2C,
-    )
-
-    # Shape of 2D filter is the same as the padded `complex64` slice shape, so the size will be
-    # the same
-    filter_size = complex_slice
-
-    # Size of cropped/unpadded + cast to float32 result of 2D IFFT
-    cropped_float32_res_slice = np.prod(non_slice_dims_shape) * np.float32().nbytes
-
-    # If the FFT plan size is negligible for some reason, this changes where the peak GPU
-    # memory usage occurs. Hence, the if/else branching below for calculating the total bytes.
-    NEGLIGIBLE_FFT_PLAN_SIZE = 16
-    if fftplan_slice_size < NEGLIGIBLE_FFT_PLAN_SIZE:
-        tot_memory_bytes = int(
-            unpadded_in_slice_size + padded_in_slice_size + complex_slice
-        )
-    else:
-        tot_memory_bytes = int(
-            unpadded_in_slice_size
-            + padded_in_slice_size
-            + complex_slice
-            # The padded float32 array is deallocated when a copy is made when casting to complex64
-            # and the variable `padded_tomo` is reassigned to the complex64 version
-            - padded_in_slice_size
-            + fftplan_slice_size
-            + cropped_float32_res_slice
-        )
-
-    return (tot_memory_bytes, filter_size)
-
 
 def _calc_memory_bytes_paganin_filter_tomopy(
     non_slice_dims_shape: Tuple[int, int],
