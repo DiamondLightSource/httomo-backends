ac404e9d28abe809bd4ab09ef9961146387919d7
Ferenc Tükör <ferenc.tukor@radwaytech.services>
2025-10-15 20:00:41 +0200

    Fix LPRec estimator on A100

--- httomo_backends/methods_database/packages/backends/httomolibgpu/supporting_funcs/recon/algorithm.py
+++ httomo_backends/methods_database/packages/backends/httomolibgpu/supporting_funcs/recon/algorithm.py
@@ -409,7 +409,7 @@ def _calc_memory_bytes_LPRec3d_tomobar(
     if min_mem_usage_filter and min_mem_usage_ifft2:
         return (tot_memory_bytes * 1.15, 4 * 1024 * 1024 + fixed_amount)
     elif min_mem_usage_filter and not min_mem_usage_ifft2:
-        return (tot_memory_bytes * 1.15, fixed_amount)
+        return (tot_memory_bytes + 60 * 1024 * 1024, fixed_amount)
     else:
         return (tot_memory_bytes * 1.1, fixed_amount)
 
