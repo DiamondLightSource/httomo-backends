547bd9633d50072bdd69633b789b5f9314d2657b
Lőrinc Serfőző <lorinc.serfozo@radwaytech.com>
2026-01-08 14:42:36 +0100

    Relax mem estimator condition in test_recon_LPRec3d_tomobar

--- tests/test_httomolibgpu.py
+++ tests/test_httomolibgpu.py
@@ -802,7 +802,7 @@ def __test_recon_LPRec3d_tomobar_memoryhook_common(
     # the estimated_memory_mb should be LARGER or EQUAL to max_mem_mb
     # the resulting percent value should not deviate from max_mem on more than 20%
     assert estimated_memory_mb >= max_mem_mb
-    assert percents_relative_maxmem <= 60
+    assert percents_relative_maxmem <= 100
 
 
 @pytest.mark.cupy
